da38450d4088665c63e521b50fd60ec6b791eda8
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2024-02-07 11:44:10 -0500

    Update index.js

--- helpers/index.js
+++ helpers/index.js
@@ -1,3 +1,3 @@
 export {default as HttpError} from './HttpError.js'
-export {default as sendEmail} from './sendMail.js'
+export {default as sendMail} from './sendMail.js'
 export {default as cloudinary} from './cloudinary.js';
