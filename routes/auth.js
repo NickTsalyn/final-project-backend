b2e7984173408c195559a5e662dc4eb44edc5dab
Ruslan <manoiloruslan@gmail.com>
2024-02-08 03:05:30 +0200

    updated swagger (Authentication+Board)
    + route and controller /users/edit updated

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ import express from "express";
 
 import { isEmptyBody, authenticate, upload, resizeAvatar } from "../middlewares/index.js";
 import validateBody from "../decorators/validateBody.js";
-import { userSignupScheme, userSigninScheme, userHelpMailScheme } from "../models/User.js";
+import { userSignupScheme, userSigninScheme, userHelpMailScheme, userEditScheme } from "../models/User.js";
 import authController from "../controllers/auth-controller.js";
 
 const authRouter = express.Router();
@@ -11,7 +11,7 @@ authRouter.post("/signup", isEmptyBody, validateBody(userSignupScheme), authCont
 authRouter.post("/signin", isEmptyBody, validateBody(userSigninScheme), authController.signin);
 authRouter.post("/signout", authenticate, authController.signout);
 authRouter.get("/current", authenticate, authController.getCurrent);
-authRouter.patch("/avatar", upload.single('avatar'), resizeAvatar, authenticate, authController.updateAvatar);
+authRouter.patch("/edit", authenticate, upload.single('avatar'), resizeAvatar, isEmptyBody, validateBody(userEditScheme), authController.editProfile);
 authRouter.post("/needHelp", authenticate, isEmptyBody, validateBody(userHelpMailScheme), authController.sendNeedHelp);
 
 // authRouter.get("/verify/:verificationToken")
--- swagger/swagger-options.js
+++ swagger/swagger-options.js
@@ -6,7 +6,7 @@ const options = {
         openapi: '3.0.1',
         info: {
             title: 'Task PRO V1 API docs',
-            version: '1.0.0',
+            version: '1.0.1',
             description: "API documentation for project 'task-pro-backend'. [Backend github repository](https://github.com/NickTsalyn/task-pro-backend)",
             license: {
                 "name": "MIT",
@@ -19,7 +19,7 @@ const options = {
     },
   
     // Paths to files with annotations
-    apis: ['./swagger/swagger-doc.js', './routes/auth.js'],
+    apis: ['./swagger/swagger-doc.js', './routes/auth.js', './routes/task.js'],
 };
 
 
