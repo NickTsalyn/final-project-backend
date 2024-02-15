c9e380a8bed2f9bcd5274f465100fb6947e08d22
Ruslan <manoiloruslan@gmail.com>
2024-02-15 15:39:20 +0200

    added checking editTask

--- swagger/swagger-options.js
+++ swagger/swagger-options.js
@@ -1,26 +1,29 @@
-import swaggerJsdoc from 'swagger-jsdoc';
-
+import swaggerJsdoc from "swagger-jsdoc";
 
 const options = {
-    definition: {
-        openapi: '3.0.1',
-        info: {
-            title: 'Task PRO V1 API docs',
-            version: '1.1.4',
-            description: "API documentation for project 'task-pro-backend'. [Backend github repository](https://github.com/NickTsalyn/task-pro-backend)",
-            license: {
-                "name": "MIT",
-                "url": "https://opensource.org/licenses/MIT"
-            }
-        },
-        servers: [
-            { url: 'https://task-pro-backend-a1c2.onrender.com' }
-        ]
+  definition: {
+    openapi: "3.0.1",
+    info: {
+      title: "Task PRO V1 API docs",
+      version: "1.2.0",
+      description:
+        "API documentation for project 'task-pro-backend'. [Backend github repository](https://github.com/NickTsalyn/task-pro-backend)",
+      license: {
+        name: "MIT",
+        url: "https://opensource.org/licenses/MIT",
+      },
     },
-  
-    // Paths to files with annotations
-    apis: ['./swagger/swagger-doc.js', './routes/auth.js', './routes/board-router.js', './routes/column-router.js', './routes/task.js'],
-};
+    servers: [{ url: "https://task-pro-backend-a1c2.onrender.com" }],
+  },
 
+  // Paths to files with annotations
+  apis: [
+    "./swagger/swagger-doc.js",
+    "./routes/auth.js",
+    "./routes/board-router.js",
+    "./routes/column-router.js",
+    "./routes/task.js",
+  ],
+};
 
 export const specs = swaggerJsdoc(options);
